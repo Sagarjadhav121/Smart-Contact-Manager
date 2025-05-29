d43adf492f8901d95b5443f4e5f7cdfc9f43e991
Sagar Jadhav <jadhavsagar121@gmail.com>
2025-05-29 13:22:05 +0530

    added tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // scm2.0/src/main/resources/static/css/input.css
+  content: ["scm2.0/src/main/resources/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: "selector",
+};
